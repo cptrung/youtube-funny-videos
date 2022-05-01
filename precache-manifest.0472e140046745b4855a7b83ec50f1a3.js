f4d73af944e7d48ae24b3a03659520eccc32c180
Trung Mèo <cptrung@gmail.com>
2022-05-01 16:26:49 +0700

    Updates

---
+++ precache-manifest.0472e140046745b4855a7b83ec50f1a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74b767995acc2e1755b1",
+    "url": "/deploy-github/static/css/main.ac0b65ca.chunk.css"
+  },
+  {
+    "revision": "74b767995acc2e1755b1",
+    "url": "/deploy-github/static/js/main.74b76799.chunk.js"
+  },
+  {
+    "revision": "a322975c65224bc82c97",
+    "url": "/deploy-github/static/js/1.a322975c.chunk.js"
+  },
+  {
+    "revision": "29d54f3a733a047cfd60",
+    "url": "/deploy-github/static/js/2.29d54f3a.chunk.js"
+  },
+  {
+    "revision": "8d8fda2c69c460b15607",
+    "url": "/deploy-github/static/js/runtime~main.8d8fda2c.js"
+  },
+  {
+    "revision": "1115a3390d390d70751c4a116ee6bc42",
+    "url": "/deploy-github/index.html"
+  }
+];
